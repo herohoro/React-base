c9274f507eead150a2c5d90af3918325b411206c
へろほろ <mtwd.office.25@gmail.com>
2022-03-22 14:51:04 +0900

    useState カウントアップボタン

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfullMessage";
 
 const App = () => {
-  const onClickButton = () => alert();
-
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  const [num, setNum] = useState(0);
   return (
     <React.Fragment>
       <h1 style={{ color: "gray" }}>こんにちは!</h1>
       <ColorfulMessage color="blue">元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">げんきですよ</ColorfulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <p>{num}</p>
     </React.Fragment>
   );
 };
