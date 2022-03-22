286710a5bfc78db34725fb87627e0fa7d5dc63e4
へろほろ <mtwd.office.25@gmail.com>
2022-03-22 15:35:29 +0900

    再レンダリングの副作用_useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,47 @@
-import React, { useState } from "react";
+// 全体で設定を宣言するならここ▼
+/* eslint react-hooks/exhaustive-deps:off */
+
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfullMessage";
 
 const App = () => {
+  //再レンダリング確認
+  // console.log("最初");
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0);
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    // console.log("effect");
+    if (num > 0) {
+      if (num % 3 === 0) {
+        // Flagがfalseならtrueに切替える
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        //Flagがtrueならfalseに切替える
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+    // esling-disable-next-line react-hooks/exhaustive-deps
+  }, [num]);
+
   return (
     <React.Fragment>
       <h1 style={{ color: "gray" }}>こんにちは!</h1>
       <ColorfulMessage color="blue">元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">げんきですよ</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
       <p>{num}</p>
+      {/* 左がtrueなら右を返す */}
+      {faceShowFlag && <p>(´・ω・｀)</p>}
     </React.Fragment>
   );
 };
--- src/components/ColorfullMessage.jsx
+++ src/components/ColorfullMessage.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 const ColorfulMessage = (props) => {
+  //再レンダリング確認
+  // console.log("カラフル");
   //分割代入_props.colorをcolorのみでOKにする
   const { color, children } = props;
   // console.log(props);
